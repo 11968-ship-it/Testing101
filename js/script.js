d6bb76bd894e423ec956f72192c13d9c0115b197
11968-ship-it <11968@holbertonstudents.com>
2026-08-20 18:10:03 +0300

    controling layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+console.log("Script loaded successfully!");
+
 document.addEventListener("DOMContentLoaded", () => {
   // Find the toggle button and navigation links
   const toggleButton = document.querySelector(".nav-toggle");
@@ -6,6 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
   // When the button is clicked, toggle the "show" class
   if (toggleButton && navLinks) {
     toggleButton.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
+      navLinks.classList.toggle("show");
+    });
+  }
+
+  // typing effect
+
+  const typingElement = document.getElementById("typing-text");
+  const text = "Hello, World!...";
+  let index = 0;
+
+  function typeText() {
+    if (index < text.length) {
+      typingElement.textContent += text.charAt(index);
+      index++;
+
+      setTimeout(typeText, 100);
+    }
+  }
+
+  if (typingElement) {
+    typeText();
+  } else {
+    console.log("ERROR: typing-text was not found!");
+  }
+
 });
